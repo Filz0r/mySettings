43826924eb072b88a5dc3595abd0f8566477462f
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-08-10 12:23:33 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Creating required folders"
+mkdir ~/.mySettings
+mkdir ~/Documents/GitHub
+mkdir ~/Documents/code
+
+echo "installing required software"
+sudo pacman -S emacs kitty nautilus discord firefox gparted \
+    kdeconnect yad gimp nvidia-settings nitrogen transmission-remote-gtk \
+    arandr xorg-xrandr bash-completion blues bpytop cmatrix cockpit cockpit-pcp \
+    nmap wireguard vim chromium evolution gnome-keyring capitaine-cursors \
+    lxappearance i3 i3lock i3-gaps neofetch syncthing stress shellcheck s-tui \
+    ttf-font-awesome ttf-nerd-fonts-symbols steam intel-undervolt iotop tree \
+    w3m vlc wireguard-tools
+
+echo "installing AUR helper and AUR packages"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -S apostrophe auto-cpufreq-git bitwarden-bin exodus github-desktop-bin i3exit \
+    js-beautify matcha-gtk-theme moka-icon-theme-git mongodb-compass nvm \
+    picom-jonaburg-git polybar polybar-spotify-module spotify tela-icon-theme \
+    timeshift vscodium
