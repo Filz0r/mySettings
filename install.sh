f80320c6832ed8d5278006ba589c1166e219f043
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-08-10 22:57:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ if [[ ! -d ~/Documents/code ]]; then mkdir ~/Documents/code; fi
 
 echo "installing required software"
 sleep 5
-sudo pacman -S emacs kitty nautilus discord firefox gparted \
+sudo pacman -S --noconfirm emacs kitty nautilus discord firefox gparted \
     kdeconnect yad gimp nvidia-settings nitrogen transmission-remote-gtk \
     arandr xorg-xrandr bash-completion bluez bpytop cmatrix cockpit cockpit-pcp \
     nmap wireguard-tools vim chromium evolution gnome-keyring capitaine-cursors \
@@ -73,6 +73,6 @@ sudo cp ./undervolt.conf /etc/intel-undervolt.conf
 sudo systemctl enable auto-cpufreq intel-undervolt tlp
 sudo intel-undervolt apply
 cp ~/.mySettings/configs/doom/* ~/.doom.d/*
-~/emacs.d/bin/doom sync
+~/.emacs.d/bin/doom sync
 
 echo "Done in $SECONDS seconds, please reboot!"
