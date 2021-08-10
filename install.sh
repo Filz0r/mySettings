8f3a22e1997f268c00fa2a92f539755af1cf360a
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-08-10 12:45:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ mkdir ~/Documents/code
 echo "installing required software"
 sudo pacman -S emacs kitty nautilus discord firefox gparted \
     kdeconnect yad gimp nvidia-settings nitrogen transmission-remote-gtk \
-    arandr xorg-xrandr bash-completion blues bpytop cmatrix cockpit cockpit-pcp \
-    nmap wireguard vim chromium evolution gnome-keyring capitaine-cursors \
+    arandr xorg-xrandr bash-completion bluez bpytop cmatrix cockpit cockpit-pcp \
+    nmap wireguard-tools vim chromium evolution gnome-keyring capitaine-cursors \
     lxappearance i3 i3lock i3-gaps neofetch syncthing stress shellcheck s-tui \
-    ttf-font-awesome ttf-nerd-fonts-symbols steam intel-undervolt iotop tree \
+    ttf-font-awesome ttf-nerd-fonts-symbols intel-undervolt iotop tree \
     w3m vlc wireguard-tools lightdm
 
 echo "installing AUR helper and AUR packages"
